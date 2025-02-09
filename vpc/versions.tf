2b4cbd4a22c6c0d9943e7f54150966f1a0d97c96
abhishekk <abhishekk@sahaj.ai>
2025-02-09 17:40:21 +0530

    Update vpc backend

--- vpc/versions.tf
+++ vpc/versions.tf
@@ -10,7 +10,7 @@ terraform {
 
   backend "s3" {
     bucket = "abhishek-infra-github"
-    key    = "infra/github/terraform.tfstate"
+    key    = "infra/github/vpc/terraform.tfstate"
     region = "ap-south-1" # terraform block don't have access to variables
     encrypt = true
   }
