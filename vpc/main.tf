cf0b1420f2f4550bbfacdb2315168aed9a83e313
abhishekk <abhishekk@sahaj.ai>
2025-03-14 12:19:59 +0530

    Create Vpc with 2 availability zones

--- vpc/main.tf
+++ vpc/main.tf
@@ -7,22 +7,41 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = var.public_subnet_cidr
-  availability_zone = "${var.aws_region}a"
-  map_public_ip_on_launch = true
+resource "aws_subnet" "public_a" {
+  vpc_id                    = aws_vpc.vpc.id
+  cidr_block                = var.public_subnet_cidr_a
+  availability_zone         = "${var.aws_region}a"
+  map_public_ip_on_launch   = true
   tags = {
-    Name = "abhishek-public-subnet"
+    Name = "abhishek-public-subnet-a"
   }
 }
 
-resource "aws_subnet" "private" {
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = var.private_subnet_cidr
-  availability_zone = "${var.aws_region}a"
+resource "aws_subnet" "public_b" {
+  vpc_id                    = aws_vpc.vpc.id
+  cidr_block                = var.public_subnet_cidr_b
+  availability_zone         = "${var.aws_region}b"
+  map_public_ip_on_launch   = true
   tags = {
-    Name = "abhishek-private-subnet"
+    Name = "abhishek-public-subnet-b"
+  }
+}
+
+resource "aws_subnet" "private_a" {
+  vpc_id                    = aws_vpc.vpc.id
+  cidr_block                = var.private_subnet_cidr_a
+  availability_zone         = "${var.aws_region}a"
+  tags = {
+    Name = "abhishek-private-subnet-a"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id                    = aws_vpc.vpc.id
+  cidr_block                = var.private_subnet_cidr_b
+  availability_zone         = "${var.aws_region}b"
+  tags = {
+    Name = "abhishek-private-subnet-b"
   }
 }
 
@@ -46,7 +65,12 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
   route_table_id = aws_route_table.public.id
 }
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,23 +1,35 @@
+variable "public_subnet_cidr_a" {
+  description = "CIDR block for the public subnet in AZ A."
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet_cidr_b" {
+  description = "CIDR block for the public subnet in AZ B."
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "private_subnet_cidr_a" {
+  description = "CIDR block for the private subnet in AZ A."
+  type        = string
+  default     = "10.0.3.0/24"
+}
+
+variable "private_subnet_cidr_b" {
+  description = "CIDR block for the private subnet in AZ B."
+  type        = string
+  default     = "10.0.4.0/24"
+}
+
 variable "aws_region" {
   description = "Region in which AWS Resource to be created"
-  type = string
-  default = "ap-south-1"
+  type        = string
+  default     = "ap-south-1"
 }
 
 variable "vpc_cidr_block" {
   description = "CIDR block for the VPC."
   type        = string
   default     = "10.0.0.0/16"
-}
-
-variable "public_subnet_cidr" {
-  description = "CIDR block for the public subnet."
-  type        = string
-  default     = "10.0.1.0/24"
-}
-
-variable "private_subnet_cidr" {
-  description = "CIDR block for the private subnet."
-  type        = string
-  default     = "10.0.2.0/24"
 }
